abceac259a7b7f39cd882a5070d389071e72e770
alexandre2297 <alexandre28200@gmail.com>
2021-06-01 15:54:06 +0200

    [TEST] Ajout de la fonction substract

--- MathTest.py
+++ MathTest.py
@@ -5,3 +5,6 @@ from Math import Math
 class MathTest(unittest.TestCase):
     def test_add(self):
         self.assertEqual(Math.add(1,1), 2)
+
+    def test_substract(self):
+        self.assertEqual(Math.substract(3.0,0.5), 2.5)
